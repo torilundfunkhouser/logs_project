38534d4ab9dcb8c8f796f7f799f814aa0840afe0
torilundfunkhouser <tori.lund.funkhouser@gmail.com>
2017-09-28 19:28:58 -0700

    added changes to views

--- news_queries.py
+++ news_queries.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#!/usr/bin/python3
 # This Python file uses the following encoding: utf-8
 
 import psycopg2
@@ -9,35 +9,34 @@ db = psycopg2.connect(database="news")
 # Returns most popular three articles of all time
 def print_top_articles():
     cursor1 = db.cursor()
-    cursor1.execute("""SELECT title FROM slug_title
-                GROUP BY title ORDER BY COUNT(title) DESC Limit 3""")
-    result = cursor1.fetchall()
-    print "The most popular articles of all time are: "
-    for result in result:
-        print "--" + str(result).translate(None, "'[](),")
+    cursor1.execute("""SELECT * from articles_count limit 3""")
+    result1 = cursor1.fetchall()
+    print "The the most popular articles of all time are: "
+    for (count, title) in result1:
+        print str(title).translate(None, "'[](),") + \
+            " -- " + str(count).translate(None, "'[](),") + " views"
 
 
 # Returns most popular three authors of all time
 def print_popular_authors():
     cursor1 = db.cursor()
-    cursor1.execute("""SELECT author_name_times.name FROM author_name_times
-                    GROUP BY author_name_times.name ORDER BY
-                    COUNT(author_name_times.name) DESC Limit 3
-                    """)
-    result = cursor1.fetchall()
-    print "The most popular articles of all time are: "
-    for result in result:
-        print "--" + str(result).translate(None, "'[](),")
+    cursor1.execute("""SELECT * from authors_count limit 3""")
+    result2 = cursor1.fetchall()
+    print "The the most popular articles of all time are: "
+    for (count, name) in result2:
+        print str(name).translate(None, "'[](),") + \
+            " -- " + str(count).translate(None, "'[](),") + " views"
 
 
 # Returns date(s) with more than 1% of GET request errors
 def print_error_days():
     cursor1 = db.cursor()
-    cursor1.execute('SELECT date from error_date')
-    result = cursor1.fetchall()
+    cursor1.execute("""SELECT * from error_date""")
+    result3 = cursor1.fetchall()
     print "On this/these date(s) more than 1% of requests returned errors: "
-    for result in result:
-        print "--" + str(result).translate(None, "'[](),")
+    for (date, percentage) in result3:
+        print str(date).translate(None, "'[](),") + \
+            " -- " + str(round(percentage, 2)).translate(None, "'[](),") + " %"
 
 # Prints results for each query
 if __name__ == '__main__':
